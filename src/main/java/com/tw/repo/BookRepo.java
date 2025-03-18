08d661bbf8197646f9af9ab8ae2f898d5220ae6b
K-Yaswanth-Sagar <yaswanthsagar0307@gmail.com>
2025-03-18 17:59:38 +0530

    implemented cuntom queries

--- src/main/java/com/tw/repo/BookRepo.java
+++ src/main/java/com/tw/repo/BookRepo.java
@@ -1,9 +1,24 @@
 package com.tw.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tw.entity.Book;
+import com.tw.entity.BookPk;
 
-public interface BookRepo extends JpaRepository<Book, Integer> {
+
+public interface BookRepo extends JpaRepository<Book, BookPk> {
+	
+	@Transactional
+	@Modifying
+	@Query("delete from Book b where b.pk.bookId = :bookId")
+	void deleteBookById(@Param("bookId") Long bookId);
+
+	
+	@Query("select b from Book b where b.pk.bookId = :bookId")
+	Book getBookById(@Param("bookId") Long bookId);
 
 }
