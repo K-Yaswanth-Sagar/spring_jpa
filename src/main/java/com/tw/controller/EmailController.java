239f82e8d594ed9240df85b34ef166875140c8a1
K-Yaswanth-Sagar <yaswanthsagar0307@gmail.com>
2025-03-26 19:05:31 +0530

    implemented Email services

---
+++ src/main/java/com/tw/controller/EmailController.java
@@ -0,0 +1,24 @@
+package com.tw.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tw.service.EmailService;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        emailService.sendSimpleEmail(to, subject, body);
+        return "Email Sent Successfully!";
+    }
+}
